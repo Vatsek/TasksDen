c503fded45027f1a89a297755dab7c4dcca1e9ca
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-14 13:36:26 +0300

    add two tasks from Denis

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*В переменной string есть секретное сообщение, во второй есть пароль. 
+Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
+
+string password = "qwerty";
+
+Console.Write("Введите пароль ");
+
+int count = 1;
+
+string secretMessage = "Угадал";
+
+string inputPassword = (Console.ReadLine());
+
+if (password == inputPassword)
+{
+    Console.WriteLine(secretMessage);
+}
+
+else
+{
+    while (count < 3)
+    {
+        Console.WriteLine("Пароль не верный");
+        count = count +1;
+        inputPassword = (Console.ReadLine());
+    }
+Console.Write("Доступ заблокирован");
+}
+
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+int count = 0;
+
+string Quit = "ex";
+
+while (Quit != "exit")
+{
+    count ++;
+    Console.WriteLine(count);
+    Quit = Console.ReadLine();
+}
+
+
+
