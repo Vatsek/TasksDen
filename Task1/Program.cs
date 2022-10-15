59a055e5f19c23fa9166445823d28a5ea44ebc97
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-16 00:44:27 +0300

    fixed the first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,31 +2,29 @@
 Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
 
 string password = "qwerty";
-
+int count = 0;
 Console.Write("Введите пароль ");
-
-int count = 1;
-
 string secretMessage = "Угадал";
+string inputPassword = "0";
 
-string inputPassword = (Console.ReadLine());
-
-if (password == inputPassword)
+while (count < 3)
+{
+    inputPassword = Console.ReadLine();
+    if (inputPassword == password)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Пароль не верный");
+        count++;
+    }
+}
+if (inputPassword == password)
 {
     Console.WriteLine(secretMessage);
 }
-
 else
 {
-    while (count < 3)
-    {
-        Console.WriteLine("Пароль не верный");
-        count = count +1;
-        inputPassword = (Console.ReadLine());
-    }
-Console.Write("Доступ заблокирован");
+    Console.WriteLine("Доступ заблокирован");
 }
-
-
-
-
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,14 +1,10 @@
 ﻿//Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
 
-int count = 0;
+string quit = "ex";
 
-string Quit = "ex";
-
-while (Quit != "exit")
+while(quit != "exit")
 {
-    count ++;
-    Console.WriteLine(count);
-    Quit = Console.ReadLine();
+    quit = Console.ReadLine();
 }
 
 
