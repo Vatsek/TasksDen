db3ac284fabf9dcc145bce9298ba6cbf6119ac6e
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-16 18:03:07 +0300

    Task 3 completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,14 +1,16 @@
-﻿/*3. Игра угадайка. Программа загадывает случайное число. 
-Пользователь его угадывает. 
+﻿/*3. Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. 
 Если пользователь дает неправильный ответ, то программа сообщает, 
-больше загаданное число или меньше. На отгадывание дается 3 попытки.
-*/
-
+больше загаданное число или меньше. На отгадывание дается 3 попытки. */
 
 int number = new Random().Next(1, 10);
 int count = 0;
 int attempt = 3;
-int inputNumber = Convert.ToInt32("");
+int inputNumber = -1;
+
+//Console.WriteLine("загаданное число " + number);
+
+Console.WriteLine("Игра началась)");
+Console.WriteLine("Введи число:");
 
 while (count < attempt)
 {
@@ -20,21 +22,20 @@ while (count < attempt)
     else if (inputNumber < number)
     {
         Console.WriteLine("Загаданное число больше");
+        count++;
     }
-    else (inputNumber > number);
+    else if (inputNumber > number)
     {
         Console.WriteLine("Загаданное число меньше");
+        count++;
     }
 
 }
 if (inputNumber == number)
 {
-    Console.WriteLine("Правильно");
+    Console.WriteLine("Правильно, ты выиграл!!!");
 }
 else
 {
-    Console.WriteLine("Доступ заблокирован");
+    Console.WriteLine("Ты проиграл((");
 }
-
-
-Console.WriteLine("загаданное число " + number);
