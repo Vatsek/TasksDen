123a5c848722b8a011c2b85766e947eb88ebd333
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-23 18:10:08 +0300

    Task4 complete

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,113 +1,164 @@
 ﻿/*Продолжаем прокачивать приложение с командами. Добавить к программе, которая заканчивается, когда пишем exit еще 4 команды (их можно придумать самому). Например:
-+SetName – Установить имя
-+Help – вывести список команд
-+SetPassword – Установить пароль
-+Exit – выход
+Exit – выход
+SetName – Установить имя
+Help – вывести список команд
+SetPassword – Установить пароль
 WriteName – вывести имя после ввода пароля
 */
 
-string inpExit = "Exit";
-string inpExitLow = inpExit.ToLower();
-string inpHelp = "Help";
-string inpSetPassword = "SetPassword";
-string inpSetName = "SetName";
-string inpWriteName = "WriteName";
-string command = "";
-string userName = "";
-string userPassword = "";
-string usrPasCheck1 = "";
-string usrPasCheck2 = "";
-string userChangePassword = "";
+string exit = "Exit";
+string command = String.Empty;
+string userName = String.Empty;
+string userPassword = String.Empty;
 
 Console.WriteLine("Готово к работе");
-while (command != inpExitLow)
+
+while (command != ToLower(exit))
 {
-        command = Console.ReadLine();
-    if (Bool(ToLower(command), ToLower(inpHelp)))
+    Console.WriteLine("Введите команду:");
+    command = ToLower(Console.ReadLine());
+    switch (command)
     {
-        Console.WriteLine($"{inpExit} – выход");
-        Console.WriteLine($"{inpSetName} – Установить имя");
-        Console.WriteLine($"{inpSetPassword} – Установить пароль");
-        Console.WriteLine($"{inpWriteName} – Вывести имя, после ввода пароля");
-    }
-    else if (Bool(ToLower(command), ToLower(inpSetName)))
-    {
-        Console.WriteLine("Введите имя: ");
-        userName = Console.ReadLine();
-        Console.WriteLine("Имя сохранено");       
-    }
-    else if (Bool(ToLower(command), ToLower(inpSetPassword)))
-    {
-        if (userPassword != "")
-        {
-            Console.WriteLine("Для смены пароля введите старый пароль");
-            userChangePassword = Console.ReadLine();
-            if (Bool (userPassword, userChangePassword))
+        case "help":
+            Console.WriteLine("Exit – выход");
+            Console.WriteLine("SetName – Установить имя пользователя");
+            Console.WriteLine("SetPassword – Установить пароль");
+            Console.WriteLine("WriteName – Вывести имя, после ввода пароля");
+            break;
+        case "setname":
+            if (userName == String.Empty)
             {
-                Console.WriteLine("Введите новый пароль");
-                usrPasCheck1 = Console.ReadLine();
-                Console.WriteLine("Подтвердите пароль");
-                usrPasCheck2 = Console.ReadLine();
-                if (Bool(usrPasCheck1, usrPasCheck2))
-                {
-                    userPassword = usrPasCheck1;
-                }
-            }    
+                Console.Write("Введите имя пользователя: ");
+                userName = Console.ReadLine();
+                Console.WriteLine("Имя пользователя сохранено.");
+                Console.WriteLine();
+                break;
+            }
             else
             {
-                Console.WriteLine("Введённые пароли не совпадают. Пароль не сохранён");
-                
-            }
-        }   
-        else 
-        {
-            Console.WriteLine("Введите пароль");
-            usrPasCheck1 = Console.ReadLine();
-            Console.WriteLine("Подтвердите пароль");
-            usrPasCheck2 = Console.ReadLine();
-        }
-        if (Bool(usrPasCheck1, usrPasCheck2))
-        {
-            Console.WriteLine("Пароль успешно сохранён.");
-            userPassword = usrPasCheck1;
-        }
-        else
-        {
-            Console.WriteLine("Введённые пароли не совпадают. Пароль не сохранён");
-        }
-    }
-    else if (Bool(ToLower(command), ToLower(inpWriteName)))
-    {
-        Console.WriteLine("Введите пароль");
-        usrPasCheck2 = Console.ReadLine();
-        if (usrPasCheck2 == userPassword && usrPasCheck2 != "")
-        {
-           Console.WriteLine($"Имя пользователя {userName}"); 
-        }
-        else
-        {
-            Console.WriteLine("Пароль не верный!"); 
-        }
-    }
-    else if (Bool(ToLower(command), ToLower(inpExitLow)))
-    {
-        return;
-    }
-    
-    else
-    {
-        Console.WriteLine("Команда не найдена. Для просмотра списка команд введите help");
-    }
+                if (userPassword == String.Empty)
+                {
+                    Console.WriteLine("Пароль не задан");
+                    goto case "setpassword";
+                }
+                else
+                {
+                    if (CheckPassword())
+                    {
+                        Console.Write("Введите новое имя пользователя: ");
+                        userName = Console.ReadLine();
+                        Console.WriteLine("Имя пользователя сохранено.");
+                        Console.WriteLine();
+                        break;
+                    }
 
-}
-bool Bool(string arg1, string arg2)
-{
-    bool result = arg1 == arg2;
-    return result;
+                    else
+                    {
+                        Console.WriteLine("Пароль не верный"); Console.WriteLine();
+                        break;
+                    }
+                }
+            }
+        case "writename":
+            if (userName == String.Empty)
+            {
+                Console.WriteLine("Имя пользователя не задано");
+                goto case "setname";
+            }
+            else
+            {
+                if (userPassword == String.Empty)
+                {
+                    Console.WriteLine("Пароль не задан");
+                    goto case "setpassword";
+                }
+                else
+                {
+                    if (CheckPassword())
+                    {
+                        Console.WriteLine($"Имя пользователя {userName}");
+                        Console.WriteLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пароль не верный"); Console.WriteLine();
+                        break;
+                    }
+                }
+            }
+        case "setpassword":
+            userPassword = ChangePassword();
+            Console.WriteLine();
+            break;
+        default:
+            Console.WriteLine("Введенной команды не существует.");
+            Console.WriteLine("Для просмотра списка команд наберите Help");
+            Console.WriteLine();
+            break;
+    }
 }
 
 string ToLower(string inpText)
 {
     string outpText = inpText.ToLower();
     return outpText;
+}
+
+string ChangePassword()
+{
+    if (userPassword == String.Empty)
+    {
+        Console.Write("Введите новый пароль: ");
+        string password1 = ToLower(Console.ReadLine());
+        Console.Write("Повторите новый пароль: ");
+        string password2 = ToLower(Console.ReadLine());
+        if (password1 == password2)
+        {
+            string password = password1;
+            Console.WriteLine("Пароль успешно сохранен!");
+            return password;
+        }
+        else
+        {
+            Console.WriteLine("Введённые пароли не совпадают. Пароль не сохранён");
+            return String.Empty;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите старый пароль");
+        string iputPassword = Console.ReadLine();
+        if (iputPassword == userPassword)
+        {
+            Console.Write("Введите новый пароль: ");
+            string password1 = ToLower(Console.ReadLine());
+            Console.Write("Повторите новый пароль: ");
+            string password2 = ToLower(Console.ReadLine());
+            if (password1 == password2)
+            {
+                string password = password1;
+                Console.WriteLine("Пароль успешно сохранен!");
+                return password;
+            }
+            else
+            {
+                Console.WriteLine("Введённые пароли не совпадают. Пароль не сохранён");
+                return userPassword;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введённые пароли не совпадают. Пароль не сохранён");
+            return userPassword;
+        }
+    }
+}
+
+bool CheckPassword()
+{
+    Console.Write("Введите пароль: ");
+    string tempPassword = Console.ReadLine();
+    bool result = tempPassword == userPassword;
+    return result;
 }
