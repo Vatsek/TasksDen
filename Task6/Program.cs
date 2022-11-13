584df93f970f47be2670fe8d552c49ee7d896419
Pavel_Vatsek <pvatsek@gmail.com>
2022-11-13 17:13:09 +0300

    add task 6 & task 7

---
+++ Task6/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Написать 2 функции для работы с массивом: AddToArray И RemoveFromArray – первая добавляет 
+к числовому массиву значение, тем самым увеличивая массив, а вторая удаляет элемент под нужным 
+индексом и уменьшает массив на 1. */
+
+int[] numbers = new int[10];
+
+FillArray(numbers);
+
+Console.WriteLine("Изначальный массив: ");
+PrintArray(numbers);
+
+
+Console.Write("Введите значение, которое хотите добавить в массив: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+numbers = AddToArray(numbers, num);
+
+Console.WriteLine("Введите индекс элемента, который хотите удалить: ");
+int indexToDel = Convert.ToInt32(Console.ReadLine());
+
+numbers = RemoveFromArray(numbers, indexToDel);
+
+
+int[] AddToArray(int[] arrayToAdd, int value)
+{
+    int size = arrayToAdd.Length;
+    int[] addArray = new int[++size];
+    for (int i = 0; i < addArray.Length - 1; i++)
+    {
+        addArray[i] = numbers[i];
+    }
+    addArray[addArray.Length - 1] = value;
+
+    Console.WriteLine("Новый увеличенный массив: ");
+    PrintArray(addArray);
+
+    return addArray;
+}
+
+
+int[] RemoveFromArray(int[] arrayFromRem, int indx)
+{
+    int size = arrayFromRem.Length;
+
+    int[] reduceArray = new int[--size];
+
+    if (indx >= arrayFromRem.Length)
+    {
+        Console.WriteLine("Такого индекса в массиве нет. Массив остался неизменным");
+        PrintArray(numbers);
+
+        return arrayFromRem;
+    }
+    else
+    {
+        for (int i = 0; i < indx; i++)
+        {
+            reduceArray[i] = arrayFromRem[i];
+        }
+        for (int i = indx; i < arrayFromRem.Length - 1; i++)
+        {
+            reduceArray[i] = arrayFromRem[i + 1];
+        }
+        Console.WriteLine("Новый уменьшенный массив: ");
+        PrintArray(reduceArray);
+        
+        return reduceArray;
+    }
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿//7. Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+
+string[] text = { "Этот", "текст","нужно","перемешать" };
+
+Random rand = new Random();
+
+WriteArray(text);
+
+text = Shuffle(text);
+
+WriteArray(text);
+
+string[] Shuffle(string[] inpText)
+{
+    for (int i = 0; i < inpText.Length; i++)
+    {
+        int randIndex = rand.Next(0, inpText.Length-1);
+        string temp = inpText[i];
+        inpText[i] = inpText[randIndex];
+        inpText[randIndex] = temp;
+    }
+    return inpText;
+}
+
+void WriteArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
